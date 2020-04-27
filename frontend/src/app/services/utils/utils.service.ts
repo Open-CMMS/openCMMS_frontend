3bfaef6e8101401327ef5d727c2f64451fc1e06d
Cleret Luc <luc.cleret@insa-rouen.fr>
2020-04-27 15:49:19 +0200

    permissionService v1 done

--- frontend/src/app/services/utils/utils.service.ts
+++ frontend/src/app/services/utils/utils.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,14 @@ import { Injectable } from '@angular/core';
 export class UtilsService {
 
   constructor() { }
+  public handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        'Backend returned code ${error.status}, ');
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 }
