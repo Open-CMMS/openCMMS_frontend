62f465d6a6026a5efd02b134f16eaa5732c0b56c
groussel <guillaume.roussel@insa-rouen.fr>
2020-04-29 16:29:16 +0200

    Ajout de la doc sur TeamManagement

--- frontend/src/app/tabs/teams/team-management/team-management.component.ts
+++ frontend/src/app/tabs/teams/team-management/team-management.component.ts
@@ -7,16 +7,24 @@ import { Router } from '@angular/router';
   templateUrl: './team-management.component.html',
   styleUrls: ['./team-management.component.scss']
 })
+/**
+ * Class for the component in charge of Teams list display
+ */
 export class TeamManagementComponent implements OnInit {
   faPlus = faPlus;
 
   constructor(private router: Router) { }
 
+  /**
+   * Function that initialize the component when loaded
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * Function that redirect to the new Team creation page
+   */
   onCreateTeam() {
-    console.log('Coucou');
     this.router.navigate(['/new-team']);
   }
 
