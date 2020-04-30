c31d368465e87922eb93d0a2ad826d112fd4c0bc
Cleret Luc <luc.cleret@insa-rouen.fr>
2020-04-30 09:40:01 +0200

    commit avant maj sur dev

--- frontend/src/app/services/auth/auth-guard.service.ts
+++ frontend/src/app/services/auth/auth-guard.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+import { UserService } from 'src/app/services/users/user.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuardService {
+@Injectable({ providedIn: 'root' })
+export class AuthGuardService implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) { }
 
-  constructor() { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.authenticationService.getCurrentUser();
+        if (currentUser) {
+            if (route.data.roles && route.data.roles.indexOf(this.userService.getUserRights()[0].getCodename()) === -1) {
+                console.log(route.data.roles);
+                this.router.navigate(['']);
+                return false;
+            }
+            return true;
+        }
+        this.router.navigate(['sign-in']);
+        return false;
+    }
 }
--- frontend/src/app/services/auth/authentication.service.ts
+++ frontend/src/app/services/auth/authentication.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { UserProfile } from 'src/app/models/user-profile';
+import { UserService } from 'src/app/services/users/user.service';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable({providedIn: 'root'})
 export class AuthenticationService {
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<UserProfile>;
+  private currentUser: Observable<UserProfile>;
+  private BASE_URL_API = environment.baseUrl;
+
+  // private currentRightList Array<Right>;
+   /**
+    * Constructor of AutheticationService
+    * @param httpClient The http instance
+    * @param userService The UserService instance
+    */
+  constructor(private httpClient: HttpClient, private userService: UserService) {
+    this.currentUserSubject = new BehaviorSubject<UserProfile>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+   /**
+    * Get the current User.
+    * @returns The current User
+    */
+  public getCurrentUser(): UserProfile {
+    return this.currentUserSubject.getValue();
+  }
+
+   /**
+    * Login method.
+    * @param userName Name provided by user.
+    * @param password Key provided by user.
+    */
+
+  public login(username: string, password: string): Observable<boolean> {
+
+    this.httpClient.post<any>(this.BASE_URL_API + 'login/', {username, password})
+      .subscribe(id => this.currentUser = this.userService.getUser(id)
+        .subscribe(user => {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }));
+    // const rep = new Observable<boolean>(response =>
+    //   response.next(username === 'jmarie' && password === 'p@sword-au-top')
+    // );
+
+    // console.log(username === 'jmarie' && password === 'p@sword-au-top');
+    // return (rep);
+
+    if (username === 'jmarie' && password === 'p@sword-au-top') {
+      this.userService.getUser(2)
+      .subscribe(user => {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+    });
+    }
+    const rep = new Observable<boolean>(response =>
+      response.next(username === 'jmarie' && password === 'p@sword-au-top')
+    );
+    return (rep);
+  }
+   /**
+    * Logout method.
+    */
+  public logout() {
+    this.httpClient.get<boolean>(`http://application.lxc.pic.brasserie-du-slalom.fr/api/gestion/logout/`)
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
 }
