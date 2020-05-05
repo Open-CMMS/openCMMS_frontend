63ec4ff78e8ce9c3cbfc4e02f4452eb08d1e13ef
Florent <florent.boulier@insa-rouen.fr>
2020-05-05 14:36:28 +0200

    TaskType service and tests

--- frontend/src/app/models/task-type.spec.ts
+++ frontend/src/app/models/task-type.spec.ts
@@ -2,6 +2,6 @@ import { TaskType } from './task-type';
 
 describe('TaskType', () => {
   it('should create an instance', () => {
-    expect(new TaskType()).toBeTruthy();
+    expect(new TaskType(0, 'Test', [1, 2], [1, 2])).toBeTruthy();
   });
 });
