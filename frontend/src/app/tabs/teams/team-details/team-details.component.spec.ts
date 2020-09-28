1de507f42e99868e7b566fe29ad47ae0693b3bc4
groussel <guillaume.roussel@insa-rouen.fr>
2020-09-28 11:08:52 +0200

    Correction des erreurs de linter

--- frontend/src/app/tabs/teams/team-details/team-details.component.spec.ts
+++ frontend/src/app/tabs/teams/team-details/team-details.component.spec.ts
@@ -69,7 +69,7 @@ describe('TeamDetailsComponent', () => {
     component.teamType = new TeamType(2, 'Test TT', [], []);
     component.initForm();
     component.onRemoveUserFromTeam(new UserProfile(1, 'Maricato', 'Hugo', 'hmaricato', 'h.m@insa-rouen.fr', 'mdp', 2, true));
-    
+
     httpTestingController.expectOne(BASE_URL_API + '/api/usersmanagement/teams/');
     httpTestingController.expectOne(BASE_URL_API + '/api/usersmanagement/teamtypes/');
     httpTestingController.expectOne(BASE_URL_API + '/api/usersmanagement/teams/NaN');
