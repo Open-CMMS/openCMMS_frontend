6152c166ddce7aa1fba11bca1b0be35073c661d1
iamodog <joran.mr@icloud.com>
2020-10-13 11:05:12 +0200

    #CU-4grcbx Avancement filtrage

--- frontend/src/app/tabs/tasks/tasks-list/tasks-list.component.ts
+++ frontend/src/app/tabs/tasks/tasks-list/tasks-list.component.ts
@@ -28,6 +28,9 @@ export class TasksListComponent implements OnInit, OnDestroy {
   currentUserSubscription: Subscription = null;
   myTasks: boolean;
 
+  //Search text
+  searchText='';
+
   /**
    * Constructor for the TasksList component
    * @param taskService the service used to handle tasks
