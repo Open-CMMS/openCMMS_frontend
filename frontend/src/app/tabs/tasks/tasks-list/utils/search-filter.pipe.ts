8480e48b4475474404e902614b0dd4741b030ffe
jo <joran.mr@gmail.com>
2020-10-14 11:02:03 +0200

    #CU-4grcbx avancement searchBar

--- frontend/src/app/tabs/tasks/tasks-list/utils/search-filter.pipe.ts
+++ frontend/src/app/tabs/tasks/tasks-list/utils/search-filter.pipe.ts
@@ -7,7 +7,7 @@ export class FilterPipe implements PipeTransform {
 
   constructor(private httpClient: HttpClient,
               private teamService: TeamService ) { }
-              
+
   transform(items: any[], searchText: string): any[] {
     if (!items) {
       return [];
@@ -16,13 +16,31 @@ export class FilterPipe implements PipeTransform {
       return items;
     }
     searchText = searchText.toLocaleLowerCase();
-    let all_teams = []
-    return items.filter(it => {
-      all_teams = []
-      it.teams.forEach(team => {
-        all_teams.push(team.name.toLocaleLowerCase())
-      });
-      return it.name.toLocaleLowerCase().includes(searchText) || all_teams.includes(searchText);
-    });
+    if (searchText[0]=="@") {
+      let searchTextWithout = searchText.substring(1);
+      let all_teams = ''
+      return items.filter( it => {
+        all_teams = ''
+        it.teams.forEach( team => {
+          all_teams = all_teams.concat(team.name.toLocaleLowerCase()+' ')
+        });
+        console.log(searchTextWithout)
+        console.log(all_teams)
+        return all_teams.includes(searchTextWithout)
+      })
+    } else {
+      return items.filter( it => {
+        return it.name.toLocaleLowerCase().includes(searchText)
+      })
+    }
+      //   let all_teams = []
+      //   return items.filter(it => {
+      //     all_teams = []
+      //     it.teams.forEach(team => {
+      //       all_teams.push(team.name.toLocaleLowerCase())
+      //     });
+      //     return it.name.toLocaleLowerCase().includes(searchText) || all_teams.includes(searchText);
+      //   })
+      // )
   }
 }
