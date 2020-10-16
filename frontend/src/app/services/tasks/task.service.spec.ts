25a877008fe2119c018234dfe051746b4df28b22
groussel <guillaume.roussel@insa-rouen.fr>
2020-10-16 15:35:25 +0200

    #CU-8enq9g - Ajout des tests pour le service Tasks

--- frontend/src/app/services/tasks/task.service.spec.ts
+++ frontend/src/app/services/tasks/task.service.spec.ts
@@ -18,271 +18,114 @@ describe('TaskService', () => {
 
     service = TestBed.inject(TaskService);
     httpTestingController = TestBed.inject(HttpTestingController);
+
+    // const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    // expect(req.request.method).toEqual('GET');
   });
 
   afterEach(() => {
     httpTestingController.verify();
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  //   const mockTasks = [
-  //     {
-  //       id: 0,
-  //       name: 'Task 0',
-  //       description: 'Description task 0',
-  //       end_date: '2020-05-05',
-  //       time: '',
-  //       is_template: true,
-  //       equipment: 0,
-  //       teams: [1, 2],
-  //       task_type: 0,
-  //       files: [1, 2],
-  //       over: true
-  //     },
-  //     {
-  //       id: 1,
-  //       name: 'Task 1',
-  //       description: 'Description task 1',
-  //       end_date: '2020-05-05',
-  //       time: '',
-  //       is_template: true,
-  //       equipment: 0,
-  //       teams: [1, 2],
-  //       task_type: 0,
-  //       files: [1, 2],
-  //       over: true
-  //     }
-  //   ];
-
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   expect(req.request.method).toEqual('GET');
-  //   req.flush(mockTasks);
-  // });
-
-  // it('should verify the parameters and content of the task get action', () => {
-  //   const mockTasks = [
-  //     {
-  //       id: 0,
-  //       name: 'Task 0',
-  //       description: 'Description task 0',
-  //       end_date: '2020-05-05',
-  //       time: '',
-  //       is_template: true,
-  //       equipment: 0,
-  //       teams: [1, 2],
-  //       task_type: 0,
-  //       files: [1, 2],
-  //       over: true
-  //     },
-  //     {
-  //       id: 1,
-  //       name: 'Task 1',
-  //       description: 'Description task 1',
-  //       end_date: '2020-05-05',
-  //       time: '',
-  //       is_template: true,
-  //       equipment: 0,
-  //       teams: [1, 2],
-  //       task_type: 0,
-  //       files: [1, 2],
-  //       over: true
-  //     }
-  //   ];
-  //   service.getTasks();
-
-  //   let tasks: Task[] = [];
-  //   service.taskSubject.subscribe(
-  //                       (tasksInService: Task[]) => {
-  //                         tasks = tasksInService;
-  //                         expect(tasks.length).toBe(2);
-  //                         expect(tasks[0].id).toBe(0);
-  //                         expect(tasks[1].id).toBe(1);
-  //                       });
-
-  //   const req = httpTestingController.match(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   expect(req[0].request.method).toEqual('GET');
-  //   expect(req[1].request.method).toEqual('GET');
-  //   req[1].flush(mockTasks);
-  // });
-
-  // it('should verify the parameters and content of the field objects get action', () => {
-  //   const mockFieldObjects = [
-  //     {
-  //       id: 1,
-  //       described_object: 'Task: 15',
-  //       field: 1,
-  //       field_value: 2,
-  //       value: '2020-05-15',
-  //       description: 'A short description'
-  //     },
-  //     {
-  //       id: 2,
-  //       described_object: 'Task: 15',
-  //       field: 1,
-  //       field_value: 2,
-  //       value: '2020-05-15',
-  //       description: 'A short description'
-  //     }
-  //   ];
-  //   service.getFieldObjects();
-
-  //   let fo: any[] = [];
-  //   service.fieldObjectSubject.subscribe(
-  //                       (foInService: any[]) => {
-  //                         fo = foInService;
-  //                         expect(fo.length).toBe(2);
-  //                         expect(fo[0].id).toBe(1);
-  //                         expect(fo[1].id).toBe(2);
-  //                       });
-
-  //   const req = httpTestingController.match(BASE_URL_API + '/api/maintenancemanagement/fieldobjects/');
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   expect(req[0].request.method).toEqual('GET');
-  //   expect(req[1].request.method).toEqual('GET');
-  //   req[1].flush(mockFieldObjects);
-  // });
-
-  it('should verify the parameters and content of the task by id GET request', () => {
-    const mockTeam = {
-      id: 1,
-      name: 'Task 1',
-      description: 'Description task 1',
-      end_date: '2020-05-05',
-      time: '',
-      is_template: true,
-      equipment: 0,
-      teams: [1, 2],
-      task_type: 0,
-      files: [1, 2],
-      over: true
-    };
+  it('should be created', () => {
+    expect(service).toBeTruthy();
     httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/fieldobjects/');
-    service.getTask(1).subscribe((task: Task) => {
-      expect(task.id).toBe(1);
-    });
-
-    const req2 = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/1/');
-    expect(req2.request.method).toEqual('GET');
-    req2.flush(mockTeam);
   });
 
-  // it('should verify the creation of a new task', () => {
-  //   const mockTask = {
-  //     id: 1,
-  //     name: 'Task 1',
-  //     description: 'Description task 1',
-  //     end_date: '2020-05-05',
-  //     time: '',
-  //     is_template: true,
-  //     equipment: 0,
-  //     teams: [1, 2],
-  //     task_type: 0,
-  //     files: [1, 2],
-  //     over: true
-  //   };
+  it('should verify the parameters and content of the task get action', () => {
+    const mockTasks = [
+      {
+        id: 0,
+        name: 'Task 0',
+        description: 'Description task 0'
+      },
+      {
+        id: 1,
+        name: 'Task 1',
+        description: 'Description task 1'
+      }
+    ];
+    service.getTasks();
 
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   const newTask = new Task(1,
-  //                           'Task 1',
-  //                           'Description',
-  //                           '',
-  //                           '',
-  //                           true,
-  //                           0,
-  //                           [1, 2],
-  //                           0,
-  //                           [1, 2],
-  //                           true);
-  //   service.createTask(newTask).subscribe(
-  //     task => {
-  //       expect(task.id).toBe(1);
-  //     }
-  //   );
+    let tasks: Task[] = [];
+    service.taskSubject.subscribe(
+                        (tasksInService: Task[]) => {
+                          tasks = tasksInService;
+                          expect(tasks.length).toBe(2);
+                          expect(tasks[0].id).toBe(0);
+                          expect(tasks[1].id).toBe(1);
+                        });
 
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   expect(req.request.method).toEqual('POST');
-  //   req.flush(mockTask);
-  // });
+    const req = httpTestingController.match(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    expect(req[0].request.method).toEqual('GET');
+    req[0].flush(mockTasks);
+  });
 
-  // it('should verify the update of a task in database', () => {
-  //   const mockTask = {
-  //     id: 1,
-  //     name: 'Task 1',
-  //     description: 'Description task 1',
-  //     end_date: '2020-05-05',
-  //     time: '',
-  //     is_template: true,
-  //     equipment: 0,
-  //     teams: [1, 2],
-  //     task_type: 0,
-  //     files: [1, 2],
-  //     over: true
-  //   };
+  it('should verify the creation of a new task', () => {
+    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    const mockTask = {
+      id: 1,
+      name: 'Task 1',
+      description: 'Description task 1'
+    };
 
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   const newTask = new Task(1,
-  //     'Task 1',
-  //     'Description',
-  //     '',
-  //     '',
-  //     true,
-  //     0,
-  //     [1, 2],
-  //     0,
-  //     [1, 2],
-  //     true);
-  //   service.updateTask(newTask.id, newTask).subscribe(
-  //     task => {
-  //       expect(task.id).toBe(1);
-  //     }
-  //   );
+    const newTask = new Task(1,
+                            'Task 1',
+                            'Description');
+    service.createTask(newTask).subscribe(
+      (task) => {
+        expect(task.id).toBe(1);
+      }
+    );
 
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/' + newTask.id + '/');
-  //   expect(req.request.method).toEqual('PUT');
-  //   req.flush(mockTask);
-  // });
+    const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    expect(req.request.method).toEqual('POST');
+    req.flush(mockTask);
+  });
 
-  // it('should verify the deletion of a task in database', () => {
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   service.deleteTask(1).subscribe();
+  it('should verify the update of a task in database', () => {
+    const mockTask = {
+      id: 1,
+      name: 'Task 1',
+      description: 'Description task 1'
+    };
 
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/1/');
-  //   expect(req.request.method).toEqual('DELETE');
-  // });
+    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    const newTask = new Task(1,
+      'Task 1',
+      'Description');
+    service.updateTask(newTask.id, newTask).subscribe(
+      task => {
+        expect(task.id).toBe(1);
+      }
+    );
 
-  // it('should verify the addition of  team to a task', () => {
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   service.addTeamToTask(1, 1).subscribe();
+    const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/' + newTask.id + '/');
+    expect(req.request.method).toEqual('PUT');
+    req.flush(mockTask);
+  });
 
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/addteamtotask');
-  //   expect(req.request.method).toEqual('POST');
-  // });
+  it('should verify the deletion of a task in database', () => {
+    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    service.deleteTask(1).subscribe();
 
-  // it('should verify the removal of a team from a task', () => {
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
-  //   service.removeTeamFromTask(1, 1).subscribe();
+    const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/1/');
+    expect(req.request.method).toEqual('DELETE');
+  });
 
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/addteamtotask');
-  //   expect(req.request.method).toEqual('PUT');
-  // });
+  it('should verify the addition of  team to a task', () => {
+    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    service.addTeamToTask(1, 1).subscribe();
 
-  // it('should verify the update request on a field object', () => {
-  //   httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/addteamtotask');
+    expect(req.request.method).toEqual('POST');
+  });
 
-  //   const mockFieldObject = {
-  //       id: 2,
-  //       described_object: 'Task: 15',
-  //       field: 1,
-  //       field_value: 2,
-  //       value: '2020-05-15',
-  //       description: 'A short description'
-  //   };
+  it('should verify the removal of a team from a task', () => {
+    httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/tasks/');
+    service.removeTeamFromTask(1, 1).subscribe();
 
-  //   service.updateFieldObject(mockFieldObject).subscribe();
-
-  //   const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/fieldobjects/2/');
-  //   expect(req.request.method).toEqual('PUT');
-  // });
+    const req = httpTestingController.expectOne(BASE_URL_API + '/api/maintenancemanagement/addteamtotask');
+    expect(req.request.method).toEqual('PUT');
+  });
 });
--- frontend/src/app/tabs/tasks/new-task/new-task.component.spec.ts
+++ frontend/src/app/tabs/tasks/new-task/new-task.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NewTaskComponent } from './new-task.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,7 +14,7 @@ describe('NewTaskComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ NewTaskComponent ],
-      imports: [ NgbModule, RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule, NgMultiSelectDropDownModule ]
+      imports: [ NgbModule, RouterTestingModule, FormsModule, HttpClientTestingModule, ReactiveFormsModule, NgMultiSelectDropDownModule ]
     })
     .compileComponents();
   }));
