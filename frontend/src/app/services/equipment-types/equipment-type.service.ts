463de8d985d13b2671762ac97df6098063c1ad1d
Billoré Jessica <jessica.billore@insa-rouen.fr>
2020-10-19 14:07:02 +0200

    #CU-8enqgm : correction linter

--- frontend/src/app/services/equipment-types/equipment-type.service.ts
+++ frontend/src/app/services/equipment-types/equipment-type.service.ts
@@ -78,7 +78,7 @@ export class EquipmentTypeService {
         (response) => {
           this.equipment_types = [];
           response.forEach(element => {
-            const gt = new EquipmentType(element.id, element.name,[]);
+            const gt = new EquipmentType(element.id, element.name, []);
             this.equipment_types.push(gt);
           });
           this.emitEquipmentTypes();
