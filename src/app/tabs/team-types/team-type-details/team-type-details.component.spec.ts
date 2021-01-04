74a83baebfe136d67a1259f9193de369b1332ea8
jbillore <jessica.billore@insa-rouen.fr>
2021-01-04 17:57:42 +0100

    #CU-bgz1zv : MAJ Tests US1

--- src/app/tabs/team-types/team-type-details/team-type-details.component.spec.ts
+++ src/app/tabs/team-types/team-type-details/team-type-details.component.spec.ts
@@ -4,7 +4,6 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TeamTypeDetailsComponent } from './team-type-details.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 
 describe('TeamTypeDetailsComponent', () => {
   let component: TeamTypeDetailsComponent;
@@ -24,7 +23,7 @@ describe('TeamTypeDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('US1_A3 - should create', () => {
     expect(component).toBeTruthy();
   });
 });
